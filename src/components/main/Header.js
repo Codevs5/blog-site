c217f32a901aea616b81ff23cb61f3511803baf0
Joseba Carral <jcarraldc@gmail.com>
2017-04-05 17:46:17 +0200

    Header designed

--- src/components/main/Header.js
+++ src/components/main/Header.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import '../../style/header.scss';
+import '../../style/__header.scss';
 
 export const Header = () => (
     <header className="header">
-        <h1>Codevs Blog
-        </h1>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+      <Link to="/" className="header-ref">
+        <img src="./static/images/logo.png" alt="Codevs" className="header-icon" height="40px" width="40px"/>
+        <h1 className="header-title"> <span>/Co</span>Devs/ </h1>
+      </Link>
     </header>
 );
